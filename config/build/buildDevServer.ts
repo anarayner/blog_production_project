37627e4fd7f7a010a0b1fab0c63aa268ba2cd9d2
Victoria <95590803+anarayner@users.noreply.github.com>
2022-09-16 11:43:23 -0400

    build_folder

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import {BuildOptions} from "./types/config";
+import type {Configuration as DevServerConfiguration} from 'webpack-dev-server'
+export function buildDevServer(options: BuildOptions): DevServerConfiguration{
+    const {port} = options
+    return {
+        port,
+        open: true,
+        historyApiFallback: true
+    }
+}
---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,37 @@
+import {RuleSetRule} from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders({isDev}: BuildOptions): RuleSetRule[]{
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev? "style-loader" : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]'
+                    },
+                }
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+            ]
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader
+    ]
+}
