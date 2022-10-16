5c9e680cd118ec9caf4f4faca47f9d5f417544dd
Victoria <95590803+anarayner@users.noreply.github.com>
2022-10-16 15:30:18 -0400

    fix modal dark theme

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -11,6 +11,7 @@ export function useTheme(): UseThemeResult {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
         setTheme(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+        document.body.className = newTheme;
     };
     return {
         theme,
