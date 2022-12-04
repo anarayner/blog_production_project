229e454b749e7d46320b881efcd36c969d8c25b7
Victoria <95590803+anarayner@users.noreply.github.com>
2022-12-04 08:22:08 -0500

    eslint plugin

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,5 +1,5 @@
 import { counterReducer } from './model/slice/CounterSlice';
-import Counter from './ui/Counter';
+import { Counter } from '../Counter/ui/Counter';
 import type { CounterSchema } from './model/types/CounterSchema';
 
 export {
